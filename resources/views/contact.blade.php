1e4eab43187944342201cd4e25339d5b935e8eee
GunturKH <guntur.kh@gmail.com>
2021-10-27 14:53:23 +0700

    fix(contact): remove selena from marketing list

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -77,7 +77,7 @@
       </div>
     </div>
     <div class="row mb-4">
-      <div class="col-sm-3">
+      <div class="col-sm-4">
         <center class="card" style="max-width: 400px; margin: 20px auto;">
           <span class="font-weight-bold">PT Glori Global Sukses</span>
           <hr />
@@ -89,7 +89,7 @@
           <small class="block" style="font-size: 14px"><a href="mailto:info@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">info@gloriglobalsukses.com</a></small>
         </center>
       </div>
-      <div class="col-sm-3">
+      <div class="col-sm-4">
         <center class="card" style="max-width: 400px; margin: 20px auto;">
           <span class="font-weight-bold">PT Glori Global Sukses</span>
           <hr />
@@ -101,55 +101,57 @@
           <small class="block" style="font-size: 14px"><a href="mailto:info@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">info@gloriglobalsukses.com</a></small>
         </center>
       </div>
+      {{--
       <div class="col-sm-3">
         <center class="card" style="max-width: 400px; margin: 20px auto;">
           <span class="font-weight-bold">PT Glori Global Sukses</span>
           <hr />
           <img class="image-responsive" src="{{url('contact/selena.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
-          <h4 class="contact-name">Selena Aprilia</h4>
-          <span class="font-italic font-weight-light">Marketing Exclusive</span>
-          <hr />
-          <div style="font-size: 16px">(+62) 822 8903 9250</div>
-          <small class="block" style="font-size: 14px"><a href="mailto:info@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">info@gloriglobalsukses.com</a></small>
-        </center>
-      </div>
-      <div class="col-sm-3">
-        <center class="card" style="max-width: 400px; margin: 20px auto;">
-          <span class="font-weight-bold">PT Glori Global Sukses</span>
-          <hr />
-          <img class="image-responsive" src="{{url('contact/christian.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
-          <h4 class="contact-name">Christian Pangalila</h4>
-          <span class="font-italic font-weight-light">Marketing Exclusive</span>
-          <hr />
-          <div style="font-size: 16px">(+62) 813 6590 5168</div>
-          <small class="block" style="font-size: 14px"><a href="mailto:info@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">info@gloriglobalsukses.com</a></small>
-        </center>
-      </div>
+      <h4 class="contact-name">Selena Aprilia</h4>
+      <span class="font-italic font-weight-light">Marketing Exclusive</span>
+      <hr />
+      <div style="font-size: 16px">(+62) 822 8903 9250</div>
+      <small class="block" style="font-size: 14px"><a href="mailto:info@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">info@gloriglobalsukses.com</a></small>
+      </center>
     </div>
-    <div class="col">
-      <table style="border-collapse:separate; border-spacing:0 9px; font-size: 18px; text-align: center; width: 100%">
-        <tr>
-          <td colspan="2">
-            <span class="icon">
-              <i class="icon-map" style="font-size: 30px; color: firebrick;"></i>
-            </span> PT Glori Global Sukses <br /> Ruko Puri Selebriti Blok A No 9 Batam - Kepulauan Riau
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <span class="icon">
-              <i class="icon-facebook" style="font-size: 30px; color: #3b5998"></i>
-            </span> <a href="https://www.facebook.com/PT-Glori-Global-Sukses-100629498187326/" target="_blank" alt="PT Glori Global Sukses" title="PT Glori Global Sukses">PT Glori Global Sukses</a>
-          </td>
-          <td>
-            <span class="icon">
-              <i class="icon-instagram" style="font-size: 30px; color: #3F729B"></i>
-            </span> <a href="https://instagram.com/gloglo.co.id?igshid=1adr2wokti7g" target="_blank" alt="PT Glori Global Sukses" title="PT Glori Global Sukses">PT Glori Global Sukses</a>
-          </td>
-        </tr>
-      </table>
+    --}}
+    <div class="col-sm-4">
+      <center class="card" style="max-width: 400px; margin: 20px auto;">
+        <span class="font-weight-bold">PT Glori Global Sukses</span>
+        <hr />
+        <img class="image-responsive" src="{{url('contact/christian.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
+        <h4 class="contact-name">Christian Pangalila</h4>
+        <span class="font-italic font-weight-light">Marketing Exclusive</span>
+        <hr />
+        <div style="font-size: 16px">(+62) 813 6590 5168</div>
+        <small class="block" style="font-size: 14px"><a href="mailto:info@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">info@gloriglobalsukses.com</a></small>
+      </center>
     </div>
   </div>
+  <div class="col">
+    <table style="border-collapse:separate; border-spacing:0 9px; font-size: 18px; text-align: center; width: 100%">
+      <tr>
+        <td colspan="2">
+          <span class="icon">
+            <i class="icon-map" style="font-size: 30px; color: firebrick;"></i>
+          </span> PT Glori Global Sukses <br /> Ruko Puri Selebriti Blok A No 9 Batam - Kepulauan Riau
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <span class="icon">
+            <i class="icon-facebook" style="font-size: 30px; color: #3b5998"></i>
+          </span> <a href="https://www.facebook.com/PT-Glori-Global-Sukses-100629498187326/" target="_blank" alt="PT Glori Global Sukses" title="PT Glori Global Sukses">PT Glori Global Sukses</a>
+        </td>
+        <td>
+          <span class="icon">
+            <i class="icon-instagram" style="font-size: 30px; color: #3F729B"></i>
+          </span> <a href="https://instagram.com/gloglo.co.id?igshid=1adr2wokti7g" target="_blank" alt="PT Glori Global Sukses" title="PT Glori Global Sukses">PT Glori Global Sukses</a>
+        </td>
+      </tr>
+    </table>
+  </div>
+</div>
 </div>
 
 @endsection
