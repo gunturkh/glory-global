7801a57537df75817979cf04bbbcd43f84965fcb
GunturKH <guntur.kh@gmail.com>
2021-11-03 14:13:04 +0700

    fix(faq & contact): change address and remove member

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -24,133 +24,149 @@
         </center>
       </div>
     </div>
+    {{--
     <div class="row mb-4" style="justify-content: center;">
       <div class="col-sm-4">
         <center class="card" style="max-width: 400px; margin: 20px auto;">
           <span class="font-weight-bold">PT Glori Global Sukses</span>
           <hr />
           <img class="image-responsive" src="{{url('contact/naomi.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
-          <h4 class="contact-name">Naomi Adista</h4>
-          <span class="font-italic font-weight-light">Marketing Manager</span>
-          <hr />
-          <div style="font-size: 16px">(+62) 822 8457 3105</div>
-          <small class="block" style="font-size: 14px"><a href="mailto:naomiadista@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">naomiadista@gloriglobalsukses.com</a></small>
-        </center>
-      </div>
-    </div>
-    <div class="row mb-4">
-      <div class="col-sm-4">
-        <center class="card" style="max-width: 400px; margin: 20px auto;">
-          <span class="font-weight-bold">PT Glori Global Sukses</span>
-          <hr />
-          <img class="image-responsive" src="{{url('contact/marco.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
-          <h4 class="contact-name">Marco Martinus</h4>
-          <span class="font-italic font-weight-light">Marketing Exclusive</span>
-          <hr />
-          <div style="font-size: 16px">(+62) 812 6849 1376</div>
-          <small class="block" style="font-size: 14px"><a href="mailto:marcomartinus@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">marcomartinus@gloriglobalsukses.com</a></small>
-        </center>
-      </div>
-      <div class="col-sm-4">
-        <center class="card" style="max-width: 400px; margin: 20px auto;">
-          <span class="font-weight-bold">PT Glori Global Sukses</span>
-          <hr />
-          <img class="image-responsive" src="{{url('contact/calista.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
-          <h4 class="contact-name">Callista Zahra</h4>
-          <span class="font-italic font-weight-light">Marketing Exclusive</span>
-          <hr />
-          <div style="font-size: 16px">(+62) 813 7178 7680</div>
-          <small class="block" style="font-size: 14px"><a href="mailto:callistazahra@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">callistazahra@gloriglobalsukses.com</a></small>
-        </center>
-      </div>
+    <h4 class="contact-name">Naomi Adista</h4>
+    <span class="font-italic font-weight-light">Marketing Manager</span>
+    <hr />
+    <div style="font-size: 16px">(+62) 822 8457 3105</div>
+    <small class="block" style="font-size: 14px"><a href="mailto:naomiadista@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">naomiadista@gloriglobalsukses.com</a></small>
+    </center>
+  </div>
+</div>
+--}}
+<div class="row mb-4">
+  <div class="col-sm-4">
+    <center class="card" style="max-width: 400px; margin: 20px auto;">
+      <span class="font-weight-bold">PT Glori Global Sukses</span>
+      <hr />
+      <img class="image-responsive" src="{{url('contact/marco.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
+      <h4 class="contact-name">Marco Martinus</h4>
+      <span class="font-italic font-weight-light">Marketing Exclusive</span>
+      <hr />
+      <div style="font-size: 16px">(+62) 812 6849 1376</div>
+      <small class="block" style="font-size: 14px"><a href="mailto:marcomartinus@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">marcomartinus@gloriglobalsukses.com</a></small>
+    </center>
+  </div>
+  <div class="col-sm-4">
+    <center class="card" style="max-width: 400px; margin: 20px auto;">
+      <span class="font-weight-bold">PT Glori Global Sukses</span>
+      <hr />
+      <img class="image-responsive" src="{{url('contact/naomi.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
+      <h4 class="contact-name">Naomi Adista</h4>
+      <span class="font-italic font-weight-light">Marketing Manager</span>
+      <hr />
+      <div style="font-size: 16px">(+62) 822 8457 3105</div>
+      <small class="block" style="font-size: 14px"><a href="mailto:naomiadista@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">naomiadista@gloriglobalsukses.com</a></small>
+    </center>
+  </div>
+  <div class="col-sm-4">
+    <center class="card" style="max-width: 400px; margin: 20px auto;">
+      <span class="font-weight-bold">PT Glori Global Sukses</span>
+      <hr />
+      <img class="image-responsive" src="{{url('contact/calista.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
+      <h4 class="contact-name">Callista Zahra</h4>
+      <span class="font-italic font-weight-light">Marketing Exclusive</span>
+      <hr />
+      <div style="font-size: 16px">(+62) 813 7178 7680</div>
+      <small class="block" style="font-size: 14px"><a href="mailto:callistazahra@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">callistazahra@gloriglobalsukses.com</a></small>
+    </center>
+  </div>
+  {{--
       <div class="col-sm-4">
         <center class="card" style="max-width: 400px; margin: 20px auto;">
           <span class="font-weight-bold">PT Glori Global Sukses</span>
           <hr />
           <img class="image-responsive" src="{{url('contact/felicia-2.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
-          <h4 class="contact-name">Felicia Eliyana</h4>
-          <span class="font-italic font-weight-light">Marketing Exclusive</span>
-          <hr />
-          <div style="font-size: 16px">(+62) 821 8191 4097</div>
-          <small class="block" style="font-size: 14px"><a href="mailto:feliciaeliyana@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">feliciaeliyana@gloriglobalsukses.com</a></small>
-        </center>
-      </div>
-    </div>
-    <div class="row mb-4">
-      <div class="col-sm-4">
-        <center class="card" style="max-width: 400px; margin: 20px auto;">
-          <span class="font-weight-bold">PT Glori Global Sukses</span>
-          <hr />
-          <img class="image-responsive" src="{{url('contact/fian.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
-          <h4 class="contact-name">Fian Pranatha</h4>
-          <span class="font-italic font-weight-light">Marketing Exclusive</span>
-          <hr />
-          <div style="font-size: 16px">(+62) 812 7634 6588</div>
-          <small class="block" style="font-size: 14px"><a href="mailto:fianpranatha@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">fianpranatha@gloriglobalsukses.com</a></small>
-        </center>
-      </div>
-      <div class="col-sm-4">
-        <center class="card" style="max-width: 400px; margin: 20px auto;">
-          <span class="font-weight-bold">PT Glori Global Sukses</span>
-          <hr />
-          <img class="image-responsive" src="{{url('contact/sandra.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
-          <h4 class="contact-name">Sandra Carren</h4>
-          <span class="font-italic font-weight-light">Marketing Exclusive</span>
-          <hr />
-          <div style="font-size: 16px">(+62) 852 7784 4252</div>
-          <small class="block" style="font-size: 14px"><a href="mailto:sandracarren@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">sandracarren@gloriglobalsukses.com</a></small>
-        </center>
-      </div>
-      {{--
+  <h4 class="contact-name">Felicia Eliyana</h4>
+  <span class="font-italic font-weight-light">Marketing Exclusive</span>
+  <hr />
+  <div style="font-size: 16px">(+62) 821 8191 4097</div>
+  <small class="block" style="font-size: 14px"><a href="mailto:feliciaeliyana@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">feliciaeliyana@gloriglobalsukses.com</a></small>
+  </center>
+</div>
+--}}
+</div>
+<div class="row mb-4">
+  <div class="col-sm-4">
+    <center class="card" style="max-width: 400px; margin: 20px auto;">
+      <span class="font-weight-bold">PT Glori Global Sukses</span>
+      <hr />
+      <img class="image-responsive" src="{{url('contact/fian.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
+      <h4 class="contact-name">Fian Pranatha</h4>
+      <span class="font-italic font-weight-light">Marketing Exclusive</span>
+      <hr />
+      <div style="font-size: 16px">(+62) 812 7634 6588</div>
+      <small class="block" style="font-size: 14px"><a href="mailto:fianpranatha@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">fianpranatha@gloriglobalsukses.com</a></small>
+    </center>
+  </div>
+  <div class="col-sm-4">
+    <center class="card" style="max-width: 400px; margin: 20px auto;">
+      <span class="font-weight-bold">PT Glori Global Sukses</span>
+      <hr />
+      <img class="image-responsive" src="{{url('contact/sandra.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
+      <h4 class="contact-name">Sandra Carren</h4>
+      <span class="font-italic font-weight-light">Marketing Exclusive</span>
+      <hr />
+      <div style="font-size: 16px">(+62) 852 7784 4252</div>
+      <small class="block" style="font-size: 14px"><a href="mailto:sandracarren@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">sandracarren@gloriglobalsukses.com</a></small>
+    </center>
+  </div>
+  {{--
       <div class="col-sm-3">
         <center class="card" style="max-width: 400px; margin: 20px auto;">
           <span class="font-weight-bold">PT Glori Global Sukses</span>
           <hr />
           <img class="image-responsive" src="{{url('contact/selena.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
-      <h4 class="contact-name">Selena Aprilia</h4>
-      <span class="font-italic font-weight-light">Marketing Exclusive</span>
-      <hr />
-      <div style="font-size: 16px">(+62) 822 8903 9250</div>
-      <small class="block" style="font-size: 14px"><a href="mailto:info@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">info@gloriglobalsukses.com</a></small>
-      </center>
-    </div>
-    --}}
-    <div class="col-sm-4">
-      <center class="card" style="max-width: 400px; margin: 20px auto;">
-        <span class="font-weight-bold">PT Glori Global Sukses</span>
-        <hr />
-        <img class="image-responsive" src="{{url('contact/christian.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
-        <h4 class="contact-name">Christian Pangalila</h4>
-        <span class="font-italic font-weight-light">Marketing Exclusive</span>
-        <hr />
-        <div style="font-size: 16px">(+62) 813 6590 5168</div>
-        <small class="block" style="font-size: 14px"><a href="mailto:christianpangalila@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">christianpangalila@gloriglobalsukses.com</a></small>
-      </center>
-    </div>
-  </div>
-  <div class="col">
-    <table style="border-collapse:separate; border-spacing:0 9px; font-size: 18px; text-align: center; width: 100%">
-      <tr>
-        <td colspan="2">
-          <span class="icon">
-            <i class="icon-map" style="font-size: 30px; color: firebrick;"></i>
-          </span> PT Glori Global Sukses <br /> Ruko Puri Selebriti Blok A No 9 Batam - Kepulauan Riau
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <span class="icon">
-            <i class="icon-facebook" style="font-size: 30px; color: #3b5998"></i>
-          </span> <a href="https://www.facebook.com/PT-Glori-Global-Sukses-100629498187326/" target="_blank" alt="PT Glori Global Sukses" title="PT Glori Global Sukses">PT Glori Global Sukses</a>
-        </td>
-        <td>
-          <span class="icon">
-            <i class="icon-instagram" style="font-size: 30px; color: #3F729B"></i>
-          </span> <a href="https://instagram.com/gloglo.co.id?igshid=1adr2wokti7g" target="_blank" alt="PT Glori Global Sukses" title="PT Glori Global Sukses">PT Glori Global Sukses</a>
-        </td>
-      </tr>
-    </table>
-  </div>
+  <h4 class="contact-name">Selena Aprilia</h4>
+  <span class="font-italic font-weight-light">Marketing Exclusive</span>
+  <hr />
+  <div style="font-size: 16px">(+62) 822 8903 9250</div>
+  <small class="block" style="font-size: 14px"><a href="mailto:info@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">info@gloriglobalsukses.com</a></small>
+  </center>
+</div>
+--}}
+<div class="col-sm-4">
+  <center class="card" style="max-width: 400px; margin: 20px auto;">
+    <span class="font-weight-bold">PT Glori Global Sukses</span>
+    <hr />
+    <img class="image-responsive" src="{{url('contact/christian.jpg')}}" style="border-radius: 50%; height: 300px; width: auto; object-fit: cover; object-position: top;">
+    <h4 class="contact-name">Christian Pangalila</h4>
+    <span class="font-italic font-weight-light">Marketing Exclusive</span>
+    <hr />
+    <div style="font-size: 16px">(+62) 813 6590 5168</div>
+    <small class="block" style="font-size: 14px"><a href="mailto:christianpangalila@gloriglobalsukses.com?Subject=Inquiry%20on%20Importir" target="_top">christianpangalila@gloriglobalsukses.com</a></small>
+  </center>
+</div>
+</div>
+<div class="col">
+  <table style="border-collapse:separate; border-spacing:0 9px; font-size: 18px; text-align: center; width: 100%">
+    <tr>
+      <td colspan="2">
+        <span class="icon">
+          <i class="icon-map" style="font-size: 30px; color: firebrick;"></i>
+        </span> PT Glori Global Sukses <br /> Ruko Puri Selebriti Blok A No 9 Batam - Kepulauan Riau
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <span class="icon">
+          <i class="icon-facebook" style="font-size: 30px; color: #3b5998"></i>
+        </span> <a href="https://www.facebook.com/PT-Glori-Global-Sukses-100629498187326/" target="_blank" alt="PT Glori Global Sukses" title="PT Glori Global Sukses">PT Glori Global Sukses</a>
+      </td>
+      <td>
+        <span class="icon">
+          <i class="icon-instagram" style="font-size: 30px; color: #3F729B"></i>
+        </span> <a href="https://instagram.com/gloglo.co.id?igshid=1adr2wokti7g" target="_blank" alt="PT Glori Global Sukses" title="PT Glori Global Sukses">PT Glori Global Sukses</a>
+      </td>
+    </tr>
+  </table>
+</div>
 </div>
 </div>
 
--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -3,88 +3,88 @@
 @include('header')
 @section('container')
 <div class="ubea-section mt-10" id="ubea-faq" data-section="faq">
-    <div class="ubea-container">
-        <div class="row justify-content-center">
-            <div class="col-md-8 text-center ubea-heading">
-                <h2>Pertanyaan yang sering ditanyakan</h2>
-                <p>Untuk calon para reseller yang ingin bergabung, ada kalanya membaca pertanyaan pertanyaan umum yang mungkin akan ditanyakan untuk memperlancar proses kegiatan import</p>
-            </div>
-        </div>
-        <div class="container">
-
-            <div class="ubea-accordion">
-                <div class="ubea-accordion-heading">
-                    <div class="icon"><i class="icon-cross"></i></div>
-                    <h3>Apa itu PT Glori Global Sukses?</h3>
-                </div>
-                <div class="ubea-accordion-content">
-                    <div class="inner" style="color: black; background: linear-gradient(90deg, #ff5126 0%,#ed8f03 100% );">
-                        <p>PT. Global Glori sukses bergerak di bidang importir (orang atau lembaga perantara dagang yang mendatangkan barang dari luar negeri)</p>
-                    </div>
-                </div>
-            </div>
-            <div class="ubea-accordion">
-                <div class="ubea-accordion-heading">
-                    <div class="icon"><i class="icon-cross"></i></div>
-                    <h3>Dimana lokasi PT Glori Global Sukses?</h3>
-                </div>
-                <div class="ubea-accordion-content">
-                    <div class="inner" style="color: black; background: linear-gradient(90deg, #ff5126 0%,#ed8f03 100% );">
-                        <p>Batam, Kota Batam, Ruko Taman Niaga Blok B No.10</p>
-                    </div>
-                </div>
-            </div>
-            <div class="ubea-accordion">
-                <div class="ubea-accordion-heading">
-                    <div class="icon"><i class="icon-cross"></i></div>
-                    <h3>Apa kelebihan import bersama PT Glori Global Sukses?</h3>
-                </div>
-                <div class="ubea-accordion-content">
-                    <div class="inner" style="color: black; background: linear-gradient(90deg, #ff5126 0%,#ed8f03 100% );">
-                        <p> * Transaksi aman, mudah dan terpercaya <br />
-                            * Harga barang identik lebih murah dari pasaran di Indonesia <br />
-                            * Apabila barang yang dikirim tidak sesuai dengan pesanan / rusak, akan kami tanggung sepenuhnya, dengan syarat dan ketentuan harus adanya video unboxing
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="ubea-accordion">
-                <div class="ubea-accordion-heading">
-                    <div class="icon"><i class="icon-cross"></i></div>
-                    <h3>Produk apa saja yang dapat diimport oleh PT Glori Global Sukses?</h3>
-                </div>
-                <div class="ubea-accordion-content">
-                    <div class="inner" style="color: black; background: linear-gradient(90deg, #ff5126 0%,#ed8f03 100% );">
-                        <p>Hampir semua produk bisa diimpor</p>
-                    </div>
-                </div>
-            </div>
-            <div class="ubea-accordion">
-                <div class="ubea-accordion-heading">
-                    <div class="icon"><i class="icon-cross"></i></div>
-                    <h3>Apakah ada biaya tambahan lainnya selain biaya import?</h3>
-                </div>
-                <div class="ubea-accordion-content">
-                    <div class="inner" style="color: black; background: linear-gradient(90deg, #ff5126 0%,#ed8f03 100% );">
-                        <p>Kami tidak memungut biaya apapun selain biaya ongkir dari Batam / Jakarta ke alamat penerima</p>
-                    </div>
-                </div>
-            </div>
-            <div class="ubea-accordion">
-                <div class="ubea-accordion-heading">
-                    <div class="icon"><i class="icon-cross"></i></div>
-                    <h3>Bagaimana jika produk yang diterima rusak atau tidak sesuai pesanan?</h3>
-                </div>
-                <div class="ubea-accordion-content">
-                    <div class="inner" style="color: black; background: linear-gradient(90deg, #ff5126 0%,#ed8f03 100% );">
-                        <p>Apabila ada produk yang rusak ataupun tidak sesuai dengan pesanan, akan kami tanggung sepenuhnya dengan syarat dan ketentuan harus adanya video unboxing</p>
-                    </div>
-                </div>
-            </div>
-
-        </div>
+  <div class="ubea-container">
+    <div class="row justify-content-center">
+      <div class="col-md-8 text-center ubea-heading">
+        <h2>Pertanyaan yang sering ditanyakan</h2>
+        <p>Untuk calon para reseller yang ingin bergabung, ada kalanya membaca pertanyaan pertanyaan umum yang mungkin akan ditanyakan untuk memperlancar proses kegiatan import</p>
+      </div>
     </div>
+    <div class="container">
+
+      <div class="ubea-accordion">
+        <div class="ubea-accordion-heading">
+          <div class="icon"><i class="icon-cross"></i></div>
+          <h3>Apa itu PT Glori Global Sukses?</h3>
+        </div>
+        <div class="ubea-accordion-content">
+          <div class="inner" style="color: black; background: linear-gradient(90deg, #ff5126 0%,#ed8f03 100% );">
+            <p>PT. Global Glori sukses bergerak di bidang importir (orang atau lembaga perantara dagang yang mendatangkan barang dari luar negeri)</p>
+          </div>
+        </div>
+      </div>
+      <div class="ubea-accordion">
+        <div class="ubea-accordion-heading">
+          <div class="icon"><i class="icon-cross"></i></div>
+          <h3>Dimana lokasi PT Glori Global Sukses?</h3>
+        </div>
+        <div class="ubea-accordion-content">
+          <div class="inner" style="color: black; background: linear-gradient(90deg, #ff5126 0%,#ed8f03 100% );">
+            <p>Ruko Puri Selebriti Blok A No.09,Jl. Tengku Sulung, Batu Besar, Kecamatan Nongsa, Kota Batam,Kepulauan Riau, 29464</p>
+          </div>
+        </div>
+      </div>
+      <div class="ubea-accordion">
+        <div class="ubea-accordion-heading">
+          <div class="icon"><i class="icon-cross"></i></div>
+          <h3>Apa kelebihan import bersama PT Glori Global Sukses?</h3>
+        </div>
+        <div class="ubea-accordion-content">
+          <div class="inner" style="color: black; background: linear-gradient(90deg, #ff5126 0%,#ed8f03 100% );">
+            <p> * Transaksi aman, mudah dan terpercaya <br />
+              * Harga barang identik lebih murah dari pasaran di Indonesia <br />
+              * Apabila barang yang dikirim tidak sesuai dengan pesanan / rusak, akan kami tanggung sepenuhnya, dengan syarat dan ketentuan harus adanya video unboxing
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="ubea-accordion">
+        <div class="ubea-accordion-heading">
+          <div class="icon"><i class="icon-cross"></i></div>
+          <h3>Produk apa saja yang dapat diimport oleh PT Glori Global Sukses?</h3>
+        </div>
+        <div class="ubea-accordion-content">
+          <div class="inner" style="color: black; background: linear-gradient(90deg, #ff5126 0%,#ed8f03 100% );">
+            <p>Hampir semua produk bisa diimpor</p>
+          </div>
+        </div>
+      </div>
+      <div class="ubea-accordion">
+        <div class="ubea-accordion-heading">
+          <div class="icon"><i class="icon-cross"></i></div>
+          <h3>Apakah ada biaya tambahan lainnya selain biaya import?</h3>
+        </div>
+        <div class="ubea-accordion-content">
+          <div class="inner" style="color: black; background: linear-gradient(90deg, #ff5126 0%,#ed8f03 100% );">
+            <p>Kami tidak memungut biaya apapun selain biaya ongkir dari Batam / Jakarta ke alamat penerima</p>
+          </div>
+        </div>
+      </div>
+      <div class="ubea-accordion">
+        <div class="ubea-accordion-heading">
+          <div class="icon"><i class="icon-cross"></i></div>
+          <h3>Bagaimana jika produk yang diterima rusak atau tidak sesuai pesanan?</h3>
+        </div>
+        <div class="ubea-accordion-content">
+          <div class="inner" style="color: black; background: linear-gradient(90deg, #ff5126 0%,#ed8f03 100% );">
+            <p>Apabila ada produk yang rusak ataupun tidak sesuai dengan pesanan, akan kami tanggung sepenuhnya dengan syarat dan ketentuan harus adanya video unboxing</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
 </div>
 
 @endsection
